5e15d7006809682ba82f1481e89eecb6c89d4b3a
Patrick Perry <patperry@gmail.com>
2010-06-28 01:43:46 +0100

    Add Newton optimization code

--- analysis/enron-static/enron-static.R
+++ analysis/enron-static/enron-static.R
@@ -196,98 +196,209 @@ Model.dynamic <- function(interval.send, interval.recv, message.set,
 }
 
 
+NegLogLik <- function(coef, static, dynamic, msg,
+                      self.loops = FALSE, derivatives = TRUE)
+{
+    fromCount <- static$fromCount
+    toCount <- static$toCount
+    coef.static <- coef[seq_len(static$varCount)]
+    coef.dynamic <- coef[static$varCount + seq_len(dynamic$varCount)]
+
+    fromMatrix <- matrix(0, msg$countWithDupes, fromCount)
+    fromMatrix[seq_len(msg$countWithDupes)
+               + msg$countWithDupes*(msg$from - 1)] <- 1
+    toMatrix <- matrix(0, msg$countWithDupes, toCount)
+    toMatrix[seq_len(msg$countWithDupes)
+             + msg$countWithDupes*(msg$to - 1)] <- 1
+
+    sendCountMatrix <- crossprod(fromMatrix, toMatrix)
+    sendCount <- as.vector(sendCountMatrix)
+
+    suff.static <- colSums((sendCount / sum(sendCount)) * static$matrix)
+    suff.dynamic <- colMeans(dynamic$matrix)
+    suff <- c(suff.static, suff.dynamic)
+
+
+    etaMatrix.static <- matrix(static$matrix %*% coef.static,
+                               fromCount, toCount)
+    etaMatrix.dynamic <- matrix(dynamic$matrix %*% coef.dynamic,
+                                ncol = toCount)
+    etaMatrix <- etaMatrix.static[msg$from,] + etaMatrix.dynamic
+
+    if (!self.loops) {
+        loop <- (seq_len(msg$countWithDupes)
+                 + (msg$from - 1) * msg$countWithDupes)
+        etaMatrix[loop] <- -Inf
+    }
+
+    weightMatrix <- exp(etaMatrix)
+    weightSum <- rowSums(weightMatrix)
+    probMatrix <- weightMatrix / weightSum
+    prob <- as.vector(probMatrix)
+
+    obs <- seq_len(msg$countWithDupes) + (msg$to - 1) * msg$countWithDupes
+    eta.obs <- etaMatrix[obs]
+    prob.obs <- probMatrix[obs]
+    resid <- log(weightSum) - eta.obs
+    nll <- mean(resid)
+
+    if (derivatives) {
+        probByFromMatrix <- crossprod(fromMatrix, probMatrix)
+        probByFrom <- as.vector(probByFromMatrix)
+
+        meanByFrom.dynamic <- array(NA, c(fromCount, toCount, dynamic$varCount))
+        dynamicArray.wt <- (prob / msg$countWithDupes) * dynamic$array
+        for (j in seq_len(toCount)) {
+            meanByFrom.dynamic[,j,] <- crossprod(fromMatrix,
+                                                 dynamicArray.wt[,j,])
+        }
+        meanByFrom.dynamic <- matrix(meanByFrom.dynamic,
+                                     ncol = dynamic$varCount)
+        rm(dynamicArray.wt) # free up space
+
+        stats.static <- cov.wt(static$matrix,
+                                wt = probByFrom,
+                                method = "ML")
+        mean.static <- stats.static$center
+        cov.static <- stats.static$cov
+
+        mean.dynamic <- colSums(meanByFrom.dynamic)
+        grad <- suff - c(mean.static, mean.dynamic)
+
+        stats.dynamic <- cov.wt(dynamic$matrix,
+                                wt = prob,
+                                method = "ML")
+        mean.dynamic <- stats.dynamic$center
+        cov.dynamic <- stats.dynamic$cov
+
+        # \sum_{ij} p_{ij} s_{ij} d_{ij}^T
+        # = \sum_j \sum_f \sum_{i : from(i) = f} p_{ij} s_{fj} d_{ij}^T
+        # = \sum_j \sum_f s_fj \sum_{i : from(i) = f} p_{ij} d_{ij}^T
+        #
+        # \sum_{ij} p_{ij} d_{ij}
+        # = \sum_j \sum_f \sum_{i : from(i) = f} p_{ij} d_{ij}^T
+        cov.cross <- crossprod(sweep(static$matrix, 2, mean.static),
+                               meanByFrom.dynamic)
+
+        fisher <- rbind(cbind(cov.static,     cov.cross),
+                        cbind(t(cov.cross), cov.dynamic))
+    }
+    
+    res <- list(nobs = msg$countWithDupes, value = nll)
+    if (derivatives) {
+        res$grad <- grad
+        res$fisher <- fisher
+    }
+        
+    res
+}
+
+
+
+
+GetStep <- function(coef, static, dynamic, msg, self.loops = FALSE,
+                    penalty = rep(1e-8, length(coef)),
+                    alpha = 0.25, beta = 0.5)
+{
+    nll <- NegLogLik(coef, static, dynamic, msg, self.loops = self.loops)
+    penalty <- penalty / nll$nobs
+    
+    nll0 <- nll$value
+    grad <- nll$grad
+    fisher <- nll$fisher
+    
+    f0 <- nll0 + 0.5 * sum(penalty * coef^2)
+    grad <- grad + penalty * coef
+    hess <- fisher
+    diag(hess) <- diag(hess) + penalty
+    chol.hess <- chol(hess)
+    search <- backsolve(chol.hess,
+                        backsolve(chol.hess, grad, transpose = TRUE))
+    dec <- as.numeric(grad %*% search)
+
+
+    t <- 1
+    done <- FALSE
+    while (!done && t > 5e-2) {
+        coef1 <- coef + t * search
+        
+        if (max(abs(coef1)) < 30) {
+            nll1 <- NegLogLik(coef1, static, dynamic, msg,
+                              self.loops = self.loops,
+                              derivatives = FALSE)$value
+            f1 <- nll1 + 0.5 * sum(penalty * coef1^2)
+
+            if (f1 <= f0 + alpha * t * dec)
+                done <- TRUE
+        }
+        
+        if (!done)
+            t <- t * beta            
+    }
+
+    step <- list(coef = coef1, search = search, size = t,
+                 decrement = dec, nloglik = nll1, nloglik.pen = f1)    
+    step
+}
+
+Fit <- function(static, dynamic, msg, self.loops = FALSE,
+                start = NULL,
+                penalty = rep(1e-8, static$varCount + dynamic$varCount),
+                control = glm.control(),
+                alpha = 0.25, beta = 0.5)
+{
+    if (is.null(start) || is.na(start)) { 
+        start <- rep(0, static$varCount + dynamic$varCount)
+    }
+
+    epsilon <- control$epsilon
+    maxit <- control$maxit
+    trace <- control$trace
+    trace <- TRUE
+
+    it <- 0
+    converged <- FALSE
+    coef0 <- start
+    
+    while (!converged && it < maxit) {
+            it <- it + 1
+            step <- GetStep(coef0, static, dynamic, msg, self.loops,
+                            penalty, alpha, beta)
+            coef1 <- step$coef
+
+            if (trace) {
+                cat("it:", it, " nloglik:", step$nloglik,
+                    " nloglik.pen:", step$nloglik.pen,
+                    " step size:", step$size,
+                    " decrement:", step$dec, "\n")
+            }
+
+            if (step$dec < epsilon)
+                converged <- TRUE
+
+            coef0 <- coef1
+        }
+    
+    if (!converged)
+        warning("failed to converged after ", it, " iterations")
+    
+    coef0
+}
+
 emp <- EmployeeSet()
 msg <- MessageSet()
 static <- Model.static(~ . - 1, ~ . - 1, emp)
 dynamic <- Model.dynamic(interval.send = 3600 * c(1, 2, 4, 8),
                          interval.recv = 3600 * c(1, 2, 4, 8),
                          msg)
-
+self.loops <- FALSE
 
 coef.static <- rnorm(static$varCount)
 coef.dynamic <- rnorm(dynamic$varCount)
+coef <- c(coef.static, coef.dynamic)
 
-fromCount <- static$fromCount
-toCount <- static$toCount
+#step <- GetStep(coef, static, dynamic, msg, self.loops)
+#step1 <- GetStep(step$coef, static, dynamic, msg, self.loops)
+#step2 <- GetStep(step1$coef, static, dynamic, msg, self.loops)
 
-fromMatrix <- matrix(0, msg$countWithDupes, fromCount)
-fromMatrix[seq_len(msg$countWithDupes)
-           + msg$countWithDupes*(msg$from - 1)] <- 1
-toMatrix <- matrix(0, msg$countWithDupes, toCount)
-toMatrix[seq_len(msg$countWithDupes)
-         + msg$countWithDupes*(msg$to - 1)] <- 1
-
-sendCountMatrix <- crossprod(fromMatrix, toMatrix)
-sendCount <- as.vector(sendCountMatrix)
-
-
-etaMatrix.static <- matrix(static$matrix %*% coef.static, fromCount, toCount)
-etaMatrix.dynamic <- matrix(dynamic$matrix %*% coef.dynamic, ncol = toCount)
-etaMatrix <- etaMatrix.static[msg$from,] + etaMatrix.dynamic
-
-
-loop <- seq_len(msg$countWithDupes) + (msg$from - 1) * msg$countWithDupes
-etaMatrix[loop] <- -Inf
-
-weightMatrix <- exp(etaMatrix)
-weightSum <- rowSums(weightMatrix)
-probMatrix <- weightMatrix / weightSum
-prob <- as.vector(probMatrix)
-
-obs <- seq_len(msg$countWithDupes) + (msg$to - 1) * msg$countWithDupes
-eta.obs <- etaMatrix[obs]
-prob.obs <- probMatrix[obs]
-resid <- log(weightSum) - eta.obs
-suff.static <- colSums((sendCount / sum(sendCount)) * static$matrix)
-suff.dynamic <- colMeans(dynamic$matrix)
-suff <- c(suff.static, suff.dynamic)
-
-
-probByFromMatrix <- crossprod(fromMatrix, probMatrix)
-probByFrom <- as.vector(probByFromMatrix)
-
-meanByFrom.dynamic <- array(NA, c(fromCount, toCount, dynamic$varCount))
-dynamicArray.wt <- (prob / msg$countWithDupes) * dynamic$array
-for (j in seq_len(toCount)) {
-    meanByFrom.dynamic[,j,] <- crossprod(fromMatrix,
-                                         dynamicArray.wt[,j,])
-}
-meanByFrom.dynamic <- matrix(meanByFrom.dynamic, ncol = dynamic$varCount)
-                            
-
-
-stats.static <- cov.wt(static$matrix,
-                        wt = probByFrom,
-                        method = "ML")
-mean.static <- stats.static$center
-cov.static <- stats.static$cov
-
-
-stats.dynamic <- cov.wt(dynamic$matrix,
-                        wt = prob,
-                        method = "ML")
-mean.dynamic <- stats.dynamic$center
-cov.dynamic <- stats.dynamic$cov
-
-
-
-# \sum_{ij} p_{ij} s_{ij} d_{ij}^T
-# = \sum_j \sum_f \sum_{i : from(i) = f} p_{ij} s_{fj} d_{ij}^T
-# = \sum_j \sum_f s_fj \sum_{i : from(i) = f} p_{ij} d_{ij}^T
-#
-# \sum_{ij} p_{ij} d_{ij}
-# = \sum_j \sum_f \sum_{i : from(i) = f} p_{ij} d_{ij}^T
-cov.cross <- crossprod(sweep(static$matrix, 2, mean.static),
-                       meanByFrom.dynamic)
-
-fisher <- rbind(cbind(cov.static,     cov.cross),
-                cbind(t(cov.cross), cov.dynamic))
-
-
-grad <- 
-
-
-
-
-
-
+fit <- Fit(static, dynamic, msg, self.loops)
