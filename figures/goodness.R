f5bb36f1254d70e3a568e1e42ccd4893612f8ed5
Patrick Perry <patperry@gmail.com>
2011-09-12 15:23:54 -0400

    Add plot of obs-by-exp

---
+++ figures/goodness.R
@@ -0,0 +1,54 @@
+# figures/goodness.R
+# ------------------
+
+
+source("code/process.R")
+
+orange <- rgb(253, 141,  60, max = 255)
+purple <- rgb(158, 154, 200, max = 255)
+col <- rgb(253, 141, 60, 128, max=255)
+palette(c(orange, purple))
+
+fit <- fromJSON("output/fit-dynamic.json")
+fit.s <- fromJSON("output/fit-static.json")
+
+nobs <- jmat(fit$observed)
+nexp <- jmat(fit$expected)
+nexp.s <- jmat(fit.s$expected)
+
+ix <- row(nobs) != col(nobs)
+nobs <- nobs[ix]
+nexp <- nexp[ix]
+nexp.s <- nexp.s[ix]
+
+resid <- (nobs - nexp) / sqrt(nexp)
+resid.s <- (nobs - nexp.s) / sqrt(nexp.s)
+
+n <- length(nexp)
+ng <- 100
+
+o <- order(nexp)
+o.s <- order(nexp.s)
+g <- 1 + floor(ng * o / (n + 1))
+g.s <- 1 + floor(ng * o.s / (n + 1))
+
+ylim <- range(c(resid, resid.s), na.rm=TRUE)
+
+
+par(mfrow=c(1, 2))
+par(mar=c(5.1, 4.1, 4.1, 1.1))
+plot(nexp.s + 1, nobs + 1, log="xy", col=col, xlab="", ylab="Observed Count + 1", main="Static")
+abline(0, 1, lty=2)
+axis(3, labels=FALSE)
+par(mar=c(5.1, 1.1, 4.1, 4.1))
+plot(nexp + 1, nobs + 1, log="xy", col=col, xlab="", ylab="", axes=FALSE, main="Static and Dynamic")
+abline(0, 1, lty=2)
+box()
+axis(1)
+axis(3, labels=FALSE)
+axis(4, labels=FALSE)
+mtext("Expected Count + 1", side=1, line=3, at=0.5)
+
+par(mfrow=c(1, 2))
+plot(order(nexp.s), resid.s, ylim=ylim, col=col)
+plot(order(nexp), resid, ylim=ylim, col=col)
