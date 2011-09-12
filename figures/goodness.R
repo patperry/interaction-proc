c36474a54663d34d9dfe0801f9c9122c6aa0a7f5
Patrick Perry <patperry@gmail.com>
2011-09-12 15:57:07 -0400

    Add plot of pearson residuals

--- figures/goodness.R
+++ figures/goodness.R
@@ -22,6 +22,7 @@ nexp <- nexp[ix]
 nexp.s <- nexp.s[ix]
 
 resid <- (nobs - nexp) / sqrt(nexp)
+resid[nexp == 0 & nobs == 0] <- 0
 resid.s <- (nobs - nexp.s) / sqrt(nexp.s)
 
 n <- length(nexp)
@@ -34,7 +35,7 @@ g.s <- 1 + floor(ng * o.s / (n + 1))
 
 ylim <- range(c(resid, resid.s), na.rm=TRUE)
 
-
+pdf("figures/nobs-by-nexp.pdf", 8, 4.5)
 par(mfrow=c(1, 2))
 par(mar=c(5.1, 4.1, 4.1, 1.1))
 plot(nexp.s + 1, nobs + 1, log="xy", col=col, xlab="", ylab="Observed Count + 1", main="Static")
@@ -48,7 +49,27 @@ axis(1)
 axis(3, labels=FALSE)
 axis(4, labels=FALSE)
 mtext("Expected Count + 1", side=1, line=3, at=0.5)
+dev.off()
 
+pdf("figures/resid-by-nexp.pdf", 8, 4.5)
 par(mfrow=c(1, 2))
-plot(order(nexp.s), resid.s, ylim=ylim, col=col)
-plot(order(nexp), resid, ylim=ylim, col=col)
+par(mar=c(5.1, 4.1, 4.1, 1.1))
+plot(order(nexp.s), resid.s, ylim=ylim, col=col, axes=FALSE, xlab="", ylab="Pearson Residual", main="Static")
+p <- seq(0, 1, len=5)
+q.s <- quantile(nexp.s, p)
+box()
+axis(1, at=n*p, labels=round(q.s, 2))
+axis(1, at=n*p, labels=paste("(", p, ")", sep=''), line=1, lwd=0)
+axis(2)
+axis(3, at=n*p, labels=FALSE)
+par(mar=c(5.1, 1.1, 4.1, 4.1))
+plot(order(nexp), resid, ylim=ylim, col=col, axes=FALSE, xlab="", ylab="", main="Static and Dynamic")
+box()
+q <- quantile(nexp, p)
+axis(1, at=n*p, labels=round(q, 2))
+axis(1, at=n*p, labels=paste("(", p, ")", sep=''), line=1, lwd=0)
+axis(3, at=n*p, labels=FALSE)
+axis(4, labels=FALSE)
+mtext("Expected Count (Quantile)", side=1, line=3.5, at=0)
+dev.off()
+
