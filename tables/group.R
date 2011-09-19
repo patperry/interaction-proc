b01124226d7721a38a3a3e6970b0ec7a0beb9689
Patrick Perry <patperry@gmail.com>
2011-09-19 16:52:33 -0400

    Fix static coefficient tables (transpose)

--- tables/group.R
+++ tables/group.R
@@ -3,8 +3,8 @@
 
 source("code/process.R")
 
-nrow <- 9
-ncol <- 10
+nrecv <- 9
+nsend <- 10
 
 fit.s <- fromJSON("output/fit-static.json")
 fit <- fromJSON("output/fit-dynamic.json")
@@ -13,36 +13,38 @@ cov.s <- get.cov(fit.s)
 cov <- get.cov(fit)
 
 tx <- get.transform()
-coef.s <- (get.coefs(fit.s) %*% tx)[1:nrow, 1:ncol]
-coef <- (get.coefs(fit) %*% tx)[1:nrow,1:ncol]
+coef.s <- (get.coefs(fit.s) %*% tx)[1:nrecv, 1:nsend]
+coef <- (get.coefs(fit) %*% tx)[1:nrecv,1:nsend]
 
 vtx <- get.transform(TRUE)
 ix0 <- rep(1:11, 12) + rep((1:12 - 1) * nrow(coef), each=11)
 cov.coef.s <- vtx %*% cov.s %*% t(vtx)
 cov.coef <- vtx %*% cov[ix0, ix0] %*% t(vtx)
-se.coef.s <- matrix(sqrt(pmax(0, diag(cov.coef.s))), 11, 12)[1:nrow, 1:ncol]
-se.coef <- matrix(sqrt(pmax(0, diag(cov.coef))), 11, 12)[1:nrow, 1:ncol]
+se.coef.s <- matrix(sqrt(pmax(0, diag(cov.coef.s))), 11, 12)[1:nrecv, 1:nsend]
+se.coef <- matrix(sqrt(pmax(0, diag(cov.coef))), 11, 12)[1:nrecv, 1:nsend]
 
 
-rownames <- list("L", "T", "J", "F", "LJ", "TJ", "LF", "TF", "JF")
-colnames <- c("1", rownames)
+recvnames <- list("L", "T", "J", "F", "LJ", "TJ", "LF", "TF", "JF")
+sendnames <- c("1", recvnames)
 
 print.coefs <- function(coef, se, include.se = TRUE) {
+    coef <- t(coef)
+    se <- t(se)
     zstat <- abs(coef / se)
     coef <- formatC(coef, format="f", digits=2)
     se <- formatC(se, format="f", digits=2)
 
-    cat("\\begin{tabular}{l", rep("r", ncol), "}\n", sep='')
+    cat("\\begin{tabular}{l", rep("r", nrecv), "}\n", sep='')
     cat("\\toprule\n")
-    cat("& \\multicolumn{", ncol, "}{c}{Receiver} \\\\\n", sep='')
-    cat("\\cmidrule(l){2-", 1 + ncol, "} \n", sep='')
-    cat("Sender", paste("& \\multicolumn{1}{c}{", colnames, "}", sep=''), "\\\\\n")
+    cat("& \\multicolumn{", nrecv, "}{c}{Receiver} \\\\\n", sep='')
+    cat("\\cmidrule(l){2-", 1 + nrecv, "} \n", sep='')
+    cat("Sender", paste("& \\multicolumn{1}{c}{", recvnames, "}", sep=''), "\\\\\n")
     cat("\\midrule\n")
-    for (i in 1:nrow) {
-        cat("\\multirow{2}{*}{", rownames[[i]], "}", sep='')
-        for (j in 1:ncol) {
+    for (i in 1:nsend) {
+        cat("\\multirow{2}{*}{", sendnames[[i]], "}", sep='')
+        for (j in 1:nrecv) {
             cat(' &')
-            if (i + 1 == j)
+            if (i == j + 1)
                 cat("\\cellcolor{Gray}")
             if (zstat[i,j] < qnorm(1 - 0.5 * 1e-3)) {
                 cat("\\textcolor{LightGray}{", coef[i,j], "}", sep='') 
@@ -52,9 +54,9 @@ print.coefs <- function(coef, se, include.se = TRUE) {
         }
         cat("\\\\\n")
         if (include.se) {
-            for (j in 1:ncol) {
+            for (j in 1:nrecv) {
                 cat(' &')
-                if (i + 1 == j)
+                if (i == j + 1)
                     cat("\\cellcolor{Gray}")
                 if (zstat[i,j] < qnorm(1 - 0.5 * 1e-3)) {
                     cat("\\textcolor{LightGray}{\\tiny{(", se[i,j], ")}}", sep='') 
